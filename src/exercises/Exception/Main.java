3fd7db2edc3d2e85d87d1f2037547b2e32bac4b7
OlegUdmurt1991 <olegborozdov199@gmail.com>
2022-11-10 18:31:06 +0400

    test on git

--- src/exercises/Exception/Main.java
+++ src/exercises/Exception/Main.java
@@ -1,7 +1,13 @@
 package exercises.Exception;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("df");
+        MyExceptionClass pro = new MyExceptionClass();
+//       String str = pro.getMessage();
+
+                Scanner in = new Scanner(System.in);
+        System.out.println(pro);
     }
 }
--- src/exercises/Exception/MyExceptionClass.java
+++ src/exercises/Exception/MyExceptionClass.java
@@ -2,28 +2,14 @@ package exercises.Exception;
 
 import java.util.Scanner;
 
-public class Exceptionly {
-    public static void main (String []args){
-
-    }
-    public static int getFactorial(int num){
-
-        int result=1;
-        try{
-            if(num<1) throw new Exception("The number is less than 1");
-
-            for(int i=1; i<=num;i++){
-
-                result*=i;
-            }
+    class MyExceptionClass extends Exception
+    {
+        public String toString()
+        {
+            return "Хватит";
         }
-        catch(Exception ex){
-
-            System.out.println(ex.getMessage());
-            result=num;
-        }
-        return result;
     }
+
 
 
 
@@ -58,7 +44,7 @@ public class Exceptionly {
 
 
 
-}
+
 //    public static void main(String[] args) throws Exception {
 ////        String[] arryName = new String[5];
 //        Scanner in = new Scanner(System.in);
